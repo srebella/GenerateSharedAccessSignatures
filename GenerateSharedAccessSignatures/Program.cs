97a79a7ce675088f9d98c03f8f5fb8d3e3e7c9fb
Santiago <Rebella>
2019-01-30 21:18:20 -0300

    Initial Commit

---
+++ GenerateSharedAccessSignatures/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+
+namespace GenerateSharedAccessSignatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            //Create the blob client object.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            //Get a reference to a container to use for the sample code, and create it if it does not exist.
+            CloudBlobContainer container = blobClient.GetContainerReference("asset-04719125-fd9e-49d2-b29c-55b4ec91be43");
+            container.CreateIfNotExists();
+
+            //Generate a SAS URI for the container, without a stored access policy.
+            //Console.WriteLine("Container SAS URI: " + GetContainerSasUri(container));
+            //Console.WriteLine();
+
+            //Generate a SAS URI for a blob within the container, without a stored access policy.
+            Console.WriteLine("Blob SAS URI: " + GetBlobSasUri(container));
+            Console.WriteLine();
+
+            //Require user input before closing the console window.
+            Console.ReadLine();
+        }
+
+        static string GetContainerSasUri(CloudBlobContainer container)
+        {
+            //Set the expiry time and permissions for the container.
+            //In this case no start time is specified, so the shared access signature becomes valid immediately.
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddMinutes(2);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Read;
+
+            //Generate the shared access signature on the container, setting the constraints directly on the signature.
+            string sasContainerToken = container.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
+        }
+        static string GetBlobSasUri(CloudBlobContainer container)
+        {
+            //Get a reference to a blob within the container.
+            CloudBlockBlob blob = container.GetBlockBlobReference("2018-02-4--20-02-28-Session 3_12_960x540_AACAudio_500.mp4");
+
+            //Upload text to the blob. If the blob does not yet exist, it will be created.
+            //If the blob does exist, its existing content will be overwritten.
+            //string blobContent = "This blob will be accessible to clients via a shared access signature (SAS).";
+            //blob.UploadText(blobContent);
+
+            //Set the expiry time and permissions for the blob.
+            //In this case, the start time is specified as a few minutes in the past, to mitigate clock skew.
+            //The shared access signature will be valid immediately.
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessStartTime = DateTimeOffset.UtcNow.AddMinutes(-5);
+            sasConstraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddMinutes(2);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Read;
+
+            //Generate the shared access signature on the blob, setting the constraints directly on the signature.
+            string sasBlobToken = blob.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return blob.Uri + sasBlobToken;
+        }
+    }
+}
